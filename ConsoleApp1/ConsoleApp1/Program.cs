843bc16d5a70a07157c1f58d6a196efc7b4f9fea
awolferret <104518827+awolferret@users.noreply.github.com>
2022-05-07 16:58:12 +0300

    1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите симовол");
+            char symbol = Convert.ToChar(Console.ReadLine());
+
+            for (int i = 0; i < name.Length+2; i++)
+            {
+                Console.Write(symbol);
+            }
+            Console.WriteLine();
+            Console.WriteLine(symbol+name+symbol);
+            for (int i = 0; i < name.Length + 2; i++)
+            {
+                Console.Write(symbol);
+            }
+        }
+    }
+}
